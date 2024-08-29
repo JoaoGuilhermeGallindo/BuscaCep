0c620cc709d23c7f5ce42751bbfe5408a525ef07
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2024-08-29 17:25:05 -0300

    deploy

---
+++ Js/app.js
@@ -0,0 +1,26 @@
+//Criar a função para coletar o cep
+
+function Coletar(){
+    let input = document.querySelector('.input-cep').value
+   //teste
+   console.log(input)
+    Dados(input)
+}
+
+//Função para consumir API Via CEP
+async function Dados(input){
+    let dados = await 
+    fetch(`https://viacep.com.br/ws/${input}/json/`).then(response => response.json())
+    console.log(dados)
+    ExibirDados(dados)
+}
+
+//Função Inserir dados na tela
+function ExibirDados(dados){
+    document.querySelector('.estado').value = dados.uf
+    document.querySelector('.cidade').value = dados.localidade
+    document.querySelector('.endereco').value = dados.logradouro
+    document.querySelector('.ddd').value = dados.ddd
+    document.querySelector('.limpar-botao').value
+    
+}
